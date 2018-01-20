094e5f3e8c86adb54d8f1c9a5b3f0fa935e5e5ab
christoffer707 <Christoffer707@gmail.com>
2018-01-20 13:31:39 -0800

    changed website

---
+++ web/java.js
@@ -0,0 +1,3 @@
+function aboutFunction() {
+    alert("My name is Christoffer Hemming\nI live in Tacoma Washington with my\n3 children. I am attending BYU-I to\nreceive my degree in software engineering.");
+}
